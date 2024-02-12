4946a9b0def83f21501930ca95844953a2a60a0d
Robin Burke <rburke2233@users.noreply.github.com>
2024-02-12 14:15:01 -0700

    Added documentation

--- scruf/agent/item_ranking_fairness.py
+++ scruf/agent/item_ranking_fairness.py
@@ -2,11 +2,15 @@ from . import FairnessMetric, FairnessMetricFactory, ItemFeatureFairnessMetric
 from abc import abstractmethod
 import scruf
 
+# Calculation is average of (for each list, 1/rank of highest ranked protected item)
 class MeanReciprocalRankFM(ItemFeatureFairnessMetric):
 
     def compute_fairness(self, history):
         pass
 
+# Calculate sum of utility for protected group items, compare to unprotected items.
+# protected group utility / unprotected utility.
+# 1 / log_2(rank + 1) (if zero-based, rank + 2)
 class DisparateExposureFM(ItemFeatureFairnessMetric):
 
     def compute_fairness(self, history):
