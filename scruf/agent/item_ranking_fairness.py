3b8df24d1f26c522df69e88fa668e2ab4bdb51e6
amandaaird <aaird1@uiowa.edu>
2024-04-08 13:17:13 -0600

    fix to utility and mrr measure

--- scruf/agent/item_ranking_fairness.py
+++ scruf/agent/item_ranking_fairness.py
@@ -2,6 +2,25 @@ from . import FairnessMetric, FairnessMetricFactory, ItemFeatureFairnessMetric
 from abc import abstractmethod
 import numpy as np
 import scruf
+class ItemFeatureFairnessMetric(FairnessMetric):
+    """
+    An ItemFeatureFairnessMetric is one where recommended items are associated with a protected
+    feature defined in the feature section.
+    """
+    _PROPERTY_NAMES = ['feature']
+
+    def __init__(self):
+        super().__init__()
+
+    def setup(self, input_props, names=None):
+        super().setup(input_props, names=self.configure_names(ItemFeatureFairnessMetric._PROPERTY_NAMES, names))
+
+    def __str__(self):
+        return f"ItemFeatureFairnessMetric: feature = {self.get_property('feature')}"
+
+    @abstractmethod
+    def compute_fairness(self, history):
+        pass
 
 # Calculation is average of (for each list, 1/rank of highest ranked protected item)
 class MeanReciprocalRankFM(ItemFeatureFairnessMetric):
@@ -10,6 +29,17 @@ class MeanReciprocalRankFM(ItemFeatureFairnessMetric):
     MRR is a statistic measure for evaluating any process that produces a list of possible responses to a sample of queries,
     ordered by probability of correctness.
     """
+    _PROPERTY_NAMES = ['target']
+
+    def __init__(self):
+        super().__init__()
+
+    def setup(self, input_props, names=None):
+        super().setup(input_props, \
+                      names=self.configure_names(MeanReciprocalRankFM._PROPERTY_NAMES, names))
+
+    def __str__(self):
+        return f"MeanReciprocalRankFM: feature = {self.get_property('feature')}"
 
     def compute_fairness(self, history):
         """
@@ -17,10 +47,13 @@ class MeanReciprocalRankFM(ItemFeatureFairnessMetric):
         The fairness is computed as the average of reciprocal ranks of the first relevant (protected) item
         encountered in the list of recommendations.
         """
+        if history.choice_output_history.is_empty():
+            return 1.0
 
         max_mrr = 0  # To track the maximum MRR across all queries
         protected_feature = self.get_property('feature')
         item_data = scruf.Scruf.state.item_features
+        target_mrr = float(self.get_property('target'))
 
         for result in history.choice_output_history.get_recent(-1):
             mrr_for_query = 0  # MRR for the current query
@@ -29,47 +62,58 @@ class MeanReciprocalRankFM(ItemFeatureFairnessMetric):
                     mrr_for_query = max(mrr_for_query, 1 / idx)
             max_mrr = max(max_mrr, mrr_for_query)
 
-        # If there are no queries or no protected items were found, max_mrr remains 0, indicating no fairness issue.
-        return max_mrr if max_mrr > 0 else 1.0
 
-
-# Calculate sum of utility for protected group items, compare to unprotected items.
-# protected group utility / unprotected utility.
-# 1 / log_2(rank + 1) (if zero-based, rank + 2)
+        return max_mrr/target_mrr
 class DisparateExposureFM(ItemFeatureFairnessMetric):
     """
     DisparateExposureFM computes fairness by ensuring that protected and non-protected groups receive
     proportional visibility/exposure in the recommendation lists, relative to their representation.
     """
+    _PROPERTY_NAMES = ['n_protected', 'target']
 
+    def __init__(self):
+        super().__init__()
+
+    def setup(self, input_props, names=None):
+        super().setup(input_props, \
+                      names=self.configure_names(DisparateExposureFM._PROPERTY_NAMES, names))
+
+    def __str__(self):
+        return f"DisparateExposureFM: feature = {self.get_property('feature')}"
     def compute_fairness(self, history):
         """
         Computes the fairness based on the concept of disparate exposure for protected and non-protected
         groups within the provided history. Fairness in this context aims to equalize the exposure of
         protected and non-protected items.
         """
-        exposure_protected = 0
-        exposure_non_protected = 0
+        if history.choice_output_history.is_empty():
+            return 1.0
+
+        utility_protected = 0
+        utility_non_protected = 0
         protected_feature = self.get_property('feature')
+        n_prot = self.get_property('n_protected')
+        n_unprot= 1-self.get_property('n_protected')
+        target = self.get_property('target')
         item_data = scruf.Scruf.state.item_features
 
-        for i, result in history.choice_output_history.get_recent(-1):
+        for result in history.choice_output_history.get_recent(-1):
             rank = 0
             for recommendation in result.get_results():
                 rank +=1
                 if item_data.is_protected(protected_feature, recommendation.item):
-                    exposure_protected += 1 / np.log2(rank + 1)
+                    utility_protected += 1 / np.log2(rank + 1)
                 else:
-                    exposure_non_protected += 1 / np.log2(rank + 1)
+                    utility_non_protected += 1 / np.log2(rank + 1)
 
         # Avoid division by zero
-        total_exposure = exposure_protected + exposure_non_protected
-        if total_exposure == 0:
-            return 1.0
+        # total_exposure = utility_protected + utility_non_protected
+        #if total_exposure == 0:
+        #    return 1.0
 
         # the proportion of exposure between protected and non-protected items has been considered.
         # Adjust this formula based on fairness definition.
-        fairness_score = exposure_protected / total_exposure
+        fairness_score = ((utility_protected/n_prot) / (utility_non_protected/n_unprot))/target
 
         return fairness_score
 
